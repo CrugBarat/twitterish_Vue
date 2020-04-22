acae41768fa92b3aefc1346f5bfb7fe2e9797c03
Craig Barratt <craigtbarratt@gmail.com>
2020-04-22 17:57:50 +0100

    adds component for tweet list

--- src/App.vue
+++ src/App.vue
@@ -37,31 +37,8 @@
 
       <section>
         <hr>
-        <div class="tweets" v-for="tweet in filteredTweets">
-          <div class="row">
-            <div class="usr-img-container">
-              <img class="user-image" v-bind:src="tweet.img" alt="">
-            </div>
-            <div class="usr-twt-container">
-              <span class="user-name">{{tweet.name}} </span>
-              <span class="user-handle">{{tweet.handle}}</span>
-              <p>{{tweet.tweet}}</p>
-            </div>
-          </div>
-          <div class="usr-likes-container">
-            <div class="row">
-              <div class="icon-space"></div>
-              <div class="icons">
-                <p><img class="icon-img-comment" src="./assets/comment.png"></p>
-              </div>
-              <div class="icons">
-                <p><button class="retweet-button" v-on:click="tweet.retweets += 1"><img class="icon-img-retweet" src="./assets/retweet.png" alt=""></button>{{tweet.retweets}}</p>
-              </div>
-              <div class="icons">
-                <p><button class="like-button" v-on:click="tweet.likes += 1"><img class="icon-img-like" src="./assets/like.png" alt=""></button>{{tweet.likes}}</p>
-              </div>
-            </div>
-          </div>
+        <div class="tweets">
+          <tweet-list-item v-for="(tweet, index) in filteredTweets" :key="index" :tweet="tweet"></tweet-list-item>
         </div>
       </section>
 
@@ -72,8 +49,13 @@
 
 
 <script>
+import TweetListItem from './components/TweetListItem.vue';
+
 export default {
   name: 'app',
+  components: {
+    'tweet-list-item': TweetListItem
+  },
   data() {
     return {
       tweets:[
@@ -221,7 +203,6 @@ input[type=text] {
   border-radius: 15%;
   background-color: #E6ECF0;
   color: #8C9AA6;
-  font-weight: bold;
   font-size: 15px;
   width: 310px;
   height: 18px;
@@ -294,11 +275,21 @@ select {
 }
 
 .menu-butn-unclicked:hover {
-    background-color: #EAF5FE;
-    color: #00acee;
+  background-color: #EAF5FE;
+  color: #00acee;
 }
 
+button {
+  background: none;
+  border: none;
+  padding: 0;
+  margin: 0;
+  border-radius: 50%;
+}
 
+button:focus {
+  outline: none;
+}
 
 /* .menu-butn-clicked {
 font-size: 18px;
@@ -317,102 +308,9 @@ color: #00acee;
   max-width: 500px;
 }
 
-.usr-img-container {
-  /* border-style: solid; */
-  float: left;
-  width: 15%;
-  margin-top: 6px;
-  padding-top: 8px;
-  padding-left: 15px;
-}
-
-.user-image {
-  height: 60px;
-  border-radius: 50%;
-}
-
-.usr-twt-container{
-  /* border-style: solid; */
-  float: left;
-  width: 75%;
-  margin-top: 6px;
-  padding-top: 8px;
-  padding-left: 8px;
-  clear: none;
-  word-wrap: break-word;
-}
-
-.user-name {
-  font-weight: 700;
-}
-
-.user-handle {
-  color: #8C9AA6;
-}
-
-.usr-likes-container {
-  /* border-style: solid; */
-  height: 35px;
-  color: #8C9AA6;
-}
-
-.icon-space {
-  /* border-style: solid; */
-  width: 17%;
-  height: 25px;
-  text-align: left;
-  float: left;
-}
-
-.icons {
-  /* border-style: solid; */
-  width: 20%;
-  height: 25px;
-  text-align: left;
-  float: left;
-}
-
-.icon-img-comment {
-  height: 18px;
-  margin-left: 10px;
-}
-
-.icon-img-retweet {
-  height: 18px;
-  margin-right: 10px;
-}
-
-.icon-img-retweet:hover {
-  background-color: #EAF5FE;
-}
-
-.icon-img-like {
-  height: 18px;
-  margin-right: 10px;
-
-}
-
-.icon-img-like:hover {
-  background-color: #EAF5FE;
-}
-
-p {
-  margin-top: 4px;
-}
-
-button {
-  background: none;
-  border: none;
-  padding: 0;
-  margin: 0;
-  border-radius: 50%;
-}
-
-button:focus {
-  outline: none;
-}
-
 /* TWEETS END */
+
+
 
 hr {
   padding: 0;
---
+++ src/components/TweetListItem.vue
@@ -0,0 +1,143 @@
+<template lang="html">
+
+  <div class="tweet-list-item">
+  <div class="row">
+    <div class="usr-img-container">
+      <img class="user-image" v-bind:src="tweet.img" alt="">
+    </div>
+    <div class="usr-twt-container">
+      <span class="user-name">{{tweet.name}} </span>
+      <span class="user-handle">{{tweet.handle}}</span>
+      <p>{{tweet.tweet}}</p>
+    </div>
+  </div>
+  <div class="usr-likes-container">
+    <div class="row">
+      <div class="icon-space"></div>
+      <div class="icons">
+        <p><img class="icon-img-comment" src="../assets/comment.png"></p>
+      </div>
+      <div class="icons">
+        <p><button class="retweet-button" v-on:click="tweet.retweets += 1"><img class="icon-img-retweet" src="../assets/retweet.png" alt=""></button>{{tweet.retweets}}</p>
+      </div>
+      <div class="icons">
+        <p><button class="like-button" v-on:click="tweet.likes += 1"><img class="icon-img-like" src="../assets/like.png" alt=""></button>{{tweet.likes}}</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'tweet-list-item',
+  props: ['tweet']
+}
+</script>
+
+<style lang="css" scoped>
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.usr-img-container {
+  /* border-style: solid; */
+  float: left;
+  width: 15%;
+  margin-top: 6px;
+  padding-top: 8px;
+  padding-left: 15px;
+}
+
+.user-image {
+  height: 60px;
+  border-radius: 50%;
+}
+
+.usr-twt-container{
+  /* border-style: solid; */
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+  padding-top: 8px;
+  padding-left: 8px;
+  clear: none;
+  word-wrap: break-word;
+}
+
+.user-name {
+  font-weight: 700;
+}
+
+.user-handle {
+  color: #8C9AA6;
+}
+
+.usr-likes-container {
+  /* border-style: solid; */
+  height: 35px;
+  color: #8C9AA6;
+}
+
+.icon-space {
+  /* border-style: solid; */
+  width: 17%;
+  height: 25px;
+  text-align: left;
+  float: left;
+}
+
+.icons {
+  /* border-style: solid; */
+  width: 20%;
+  height: 25px;
+  text-align: left;
+  float: left;
+}
+
+.icon-img-comment {
+  height: 18px;
+  margin-left: 10px;
+}
+
+.icon-img-retweet {
+  height: 18px;
+  margin-right: 10px;
+}
+
+.icon-img-retweet:hover {
+  background-color: #EAF5FE;
+}
+
+.icon-img-like {
+  height: 18px;
+  margin-right: 10px;
+
+}
+
+.icon-img-like:hover {
+  background-color: #EAF5FE;
+}
+
+p {
+  margin-top: 4px;
+}
+
+button {
+  background: none;
+  border: none;
+  padding: 0;
+  margin: 0;
+  border-radius: 50%;
+}
+
+button:focus {
+  outline: none;
+}
+
+
+</style>
